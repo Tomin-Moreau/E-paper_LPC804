c539113d31d91eb37d3bcc7ab3a8e12ef6cd9ecb
moreau.tomin <moreau.tomin@gmail.com>
2023-12-03 13:15:09 +0100

    Added LPC804 starting base(arduino)

---
+++ Projet_e_paper/src/e_paper.c
@@ -0,0 +1,15 @@
+
+#include "LPC8xx.h"
+#include <stdio.h>
+#include "syscon.h"
+#include "rom_api.h"
+#include "swm.h"
+
+
+int main(void) {
+
+	while (1) {
+
+
+	} // end of while(1)
+} // end of main
